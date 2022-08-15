82eb14e6e635771ba48227eef6ad2fd86ff001dd
NikitaNino <nikita-vip1904@yandex.ru>
2022-08-15 22:19:19 +0300

    version 1.0.1

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,25 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User Anton = new User("Антон", "Антонович", "AntonA@mail.ru");
+      User Nikita =  new User("Никита", "Никитович", "NikitaN2@mail.ru");
+      User Oleg = new User("Олег", "Олегович", "OlegO@mail.ru");
+      User Denis = new User("Денис", "Денисович", "DenisD@mail.ru");
+
+      Anton.setCar(new Car("LADA", 2114));
+      Nikita.setCar(new Car("Opel", 5));
+      Oleg.setCar(new Car("Reno", 703));
+      Denis.setCar(new Car("KIA", 300));
+
+      userService.add(Anton);
+      userService.add(Nikita);
+      userService.add(Oleg);
+      userService.add(Denis);
+
+//      Car Lada = new Car("LADA", 2114);
+//      Car Opel = new Car("Opel", 5);
+//      Car Reno = new Car("Reno", 703);
+//      Car Kia = new Car("KIA", 300);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +44,14 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+//
+//      userService.add(Anton.getCar(Lada).getUser(Anton));
+//      userService.add(Nikita.getCar(Opel).getUser(Nikita));
+//      userService.add(Oleg.getCar(Reno).getUser(Oleg));
+//      userService.add(Denis.getCar(Kia).getUser(Denis));
+//
+      System.out.println(userService.listUsers());
+      System.out.println(userService.getUserByCar("Reno", 703));
 
       context.close();
    }
